4c2d40e4192a53f1155c2ae59f27bde3ec8b0946
Tiago Berwanger <berwangertiago@gmail.com>
2021-06-12 14:08:12 -0300

    requirement 2 - pt. 1

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,12 +1,24 @@
+from collections import Counter
+
+
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append([costumer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        client_orders = []
+        if len(self.orders) > 0:
+            for item in self.orders:
+                if item[0] == costumer:
+                    client_orders.append(item[1])
+            favorite = Counter(client_orders).most_common(1)[0][0]
+            return favorite
 
     def get_order_frequency_per_costumer(self, costumer, order):
         pass
