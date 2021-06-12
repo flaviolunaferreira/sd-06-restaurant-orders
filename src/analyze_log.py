03ff821179958750f52b00487a2d679b48673bc1
ricknois <rickssti@outlook.com>
2021-06-12 10:21:48 -0300

    Req 01

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,71 @@
+import csv
+
+
+def read_csv(path):
+    with open(path) as file:
+        content = list(csv.reader(file))
+    return content
+
+
+def most_requested_dish(requests, people):
+    orders = {}
+    for request in requests:
+        if request[0] == people and request[1] not in orders:
+            orders[request[1]] = 1
+        elif request[0] == people:
+            orders[request[1]] += 1
+    return max(orders, key=orders.get)
+
+
+def count_repeated_dish(requests, people, food):
+    orders = 0
+    for request in requests:
+        if request[0] == people and request[1] == food:
+            orders += 1
+    return orders
+
+
+def dishes_never_ordered(requests, people):
+    all_dishes = set()
+    dishes = set()
+    for request in requests:
+        all_dishes.add(request[1])
+        if request[0] == people:
+            dishes.add(request[1])
+    return all_dishes.difference(dishes)
+
+
+def days(requests, people):
+    all_days = set()
+    day = set()
+    for request in requests:
+        all_days.add(request[2])
+        if request[0] == people:
+            day.add(request[2])
+    return all_days.difference(day)
+
+
+def write_txt(path, data):
+    with open(path, 'w') as file:
+        for data in data:
+            file.write(str(data) + '\n')
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    data = []
+    txt_path = 'data/mkt_campaign.txt'
+    requests = read_csv(path_to_file)
+
+    data.append(most_requested_dish(requests, 'maria'))
+    data.append(count_repeated_dish(requests, 'arnaldo', 'hamburguer'))
+    data.append(dishes_never_ordered(requests, 'joao'))
+    data.append(days(requests, 'joao'))
+
+    write_txt(txt_path, data)
+
+    return data
+
+
+if __name__ == "__main__":
+    file = analyze_log('data/orders_1.csv')
+    print(file)
