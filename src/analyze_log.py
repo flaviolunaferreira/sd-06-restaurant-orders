f2393d1c07f0b538bbe990f7259d1c73a6038323
higoramorim <1810042@aluno.univesp.br>
2021-06-12 18:25:11 -0300

    comecando

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,9 @@
 def analyze_log(path_to_file):
     raise NotImplementedError
+# Qual o prato mais pedido por 'maria'?
+
+# Quantas vezes 'arnaldo' pediu 'hamburguer'?
+
+# Quais pratos 'joao' nunca pediu?
+
+# Quais dias 'joao' nunca foi na lanchonete?
