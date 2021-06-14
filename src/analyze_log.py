3b573d02eafbb8b9177ba362f463b770f76dbd91
AlexanderFeitosa <alexander.feitosa@gmail.com>
2021-06-14 00:27:33 -0300

    :bug: fix(src): fixing analyze_log complexity

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -8,57 +8,63 @@ def analyze_log(path_to_file):
         for row in csv_data:
             data.append(row.split(","))
 
-    maria_meals = []
-
-    for item in data:
-        if item[0] == "maria":
-            maria_meals.append(item[1])
+    maria_meals = new_list(data, "maria")
 
     maria_favorite = max(set(maria_meals), key=maria_meals.count)
 
-    arnaldo_meals = []
-
-    for item in data:
-        if item[0] == "arnaldo":
-            arnaldo_meals.append(item[1])
+    arnaldo_meals = new_list(data, "arnaldo")
 
     arnaldo_hamburguer = arnaldo_meals.count("hamburguer")
 
-    joao_meals = []
+    joao_meals = new_list(data, "joao")
     not_ordered = set()
 
-    for item in data:
-        if item[0] == "joao":
-            joao_meals.append(item[1])
-
-    all_meals = []
-    for item in data:
-        if item[1] not in all_meals:
-            all_meals.append(item[1])
+    all_meals = find_meals(data)
 
     for meal in all_meals:
         if meal not in joao_meals:
             not_ordered.add(meal)
 
-    dias = [
-        "segunda-feira",
-        "terça-feira",
-        "sabado",
-    ]
-
-    joao_absent = set()
-    joao_present = []
-
-    for item in data:
-        if item[0] == "joao":
-            joao_present.append(item[2].replace("\n", ""))
-
-    for dia in dias:
-        if dia not in joao_present:
-            joao_absent.add(dia)
+    joao_absent = find_days_away(data, "joao")
 
     with open("../sd-06-restaurant-orders/data/mkt_campaign.txt", "w") as file:
         file.write(maria_favorite + "\n")
         file.write(str(arnaldo_hamburguer) + "\n")
         file.write(str(not_ordered) + "\n")
         file.write(str(joao_absent))
+
+
+def new_list(list1, name):
+    result_list = []
+
+    for item in list1:
+        if item[0] == name:
+            result_list.append(item[1])
+
+    return result_list
+
+
+def find_meals(meals):
+    meals_list = set()
+
+    for item in meals:
+        if item[1] not in meals_list:
+            meals_list.add(item[1])
+
+    return meals_list
+
+
+def find_days_away(list1, name):
+    days_away = set()
+
+    dias = {
+        "segunda-feira",
+        "terça-feira",
+        "sabado",
+    }
+
+    for item in list1:
+        if item[0] == name and item[2].replace("\n", "") not in days_away:
+            days_away.add(item[2].replace("\n", ""))
+
+    return dias.difference(days_away)
