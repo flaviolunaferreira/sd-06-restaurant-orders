59dacd2ba0a28c3329227bee90edc7daa63c4722
Gustavo Oliva <gustavociviloliva@gmail.com>
2021-06-14 20:09:42 -0300

    :construction: finishing mandatory requirements

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,33 +1,38 @@
 import csv
 
 
+def check_and_make_orders(client, order, day, orders):
+    if (client not in orders.keys()):
+        orders[client] = {"day": set(), "all_orders": dict()}
+
+    if order not in orders[client]["all_orders"].keys():
+        orders[client]["all_orders"][order] = 1
+    else:
+        orders[client]["all_orders"][order] += 1
+    orders[client]["day"].add(day)
+
+
+def write_txt_file(object, orders, dishes, joao_dishes, all_days):
+    with open("data/mkt_campaign.txt", "w") as txt_file:
+        txt_file.write(f"{max(object, key=object.get)}\n")
+        txt_file.write(f"{orders['arnaldo']['all_orders']['hamburguer']}\n")
+        txt_file.write(f"{dishes - joao_dishes}\n")
+        txt_file.write(f"{all_days - orders['joao']['day']}\n")
+
+
 def analyze_log(path_to_file):
-    orders = {
-        "maria": {"day": set(), "all_orders": dict()},
-        "arnaldo": {"day": set(), "all_orders": dict()},
-        "joao": {"day": set(), "all_orders": dict()},
-        "jose": {"day": set(), "all_orders": dict()},
-    }
+    orders = dict()
     dishes = set()
     all_days = set()
     with open(path_to_file, newline="") as csv_file:
         reader = csv.reader(csv_file)
-        print(reader)
+
         for row in reader:
             client, order, day = row
             dishes.add(order)
             all_days.add(day)
+            check_and_make_orders(client, order, day, orders)
 
-            if order not in orders[client]["all_orders"].keys():
-                orders[client]["all_orders"][order] = 1
-            else:
-                orders[client]["all_orders"][order] += 1
-            orders[client]["day"].add(day)
     maria_object = orders["maria"]["all_orders"]
-    with open("data/mkt_campaign.txt", "w") as txt_file:
-        txt_file.write(f"{max(maria_object, key=maria_object.get)}\n")
-        txt_file.write(f"{orders['arnaldo']['all_orders']['hamburguer']}\n")
-        txt_file.write(
-            f"{dishes - set(orders['joao']['all_orders'].keys())}\n"
-        )
-        txt_file.write(f"{all_days - orders['joao']['day']}\n")
+    joao_dishes = set(orders['joao']['all_orders'].keys())
+    write_txt_file(maria_object, orders, dishes, joao_dishes, all_days)
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,24 +1,71 @@
 class TrackOrders:
+    def __init__(self):
+        self.orders = dict()
+        self.quantity = 0
+        self.all_days = set()
+        self.dishes = set()
+        self.busiest_day = set()
+        self.least_busy_day = set()
+
     def __len__(self):
-        pass
+        return self.quantity
 
     def add_new_order(self, costumer, order, day):
-        pass
+        if costumer not in self.orders.keys():
+            self.orders[costumer] = {"all_orders": dict(), "days": dict()}
+        if order not in self.orders[costumer]["all_orders"].keys():
+            self.orders[costumer]["all_orders"][order] = 1
+        else:
+            self.orders[costumer]["all_orders"][order] += 1
+        if day not in self.orders[costumer]["days"].keys():
+            self.orders[costumer]["days"][day] = 1
+        else:
+            self.orders[costumer]["days"][day] += 1
+        self.quantity += 1
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        costumer_orders = self.orders[costumer]["all_orders"]
+        return max(costumer_orders, key=costumer_orders.get)
 
     def get_order_frequency_per_costumer(self, costumer, order):
         pass
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        costumer_dish = set()
+        for client, value in self.orders.items():
+            self.dishes = self.dishes.union(set(value["all_orders"].keys()))
+            if client == costumer:
+                costumer_dish = costumer_dish.union(
+                    set(value["all_orders"].keys())
+                )
+        return self.dishes - costumer_dish
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        days_per_client = set()
+        for client, value in self.orders.items():
+            self.all_days = self.all_days.union(set(value["days"].keys()))
+            if client == costumer:
+                days_per_client = days_per_client.union(
+                    set(value["days"].keys())
+                )
+        return self.all_days - days_per_client
 
     def get_busiest_day(self):
-        pass
+        maximum_quantity = 0
+        for client, value in self.orders.items():
+            maximum_day = max(value["days"], key=value["days"].get)
+
+            if value["days"][maximum_day] > maximum_quantity:
+                self.busiest_day = maximum_day
+                maximum_quantity = value["days"][maximum_day]
+        return self.busiest_day
 
     def get_least_busy_day(self):
-        pass
+        minimum_quantity = 1000
+        for client, value in self.orders.items():
+            minimum_day = min(value["days"], key=value["days"].get)
+
+            if value["days"][minimum_day] < minimum_quantity:
+                self.least_day = minimum_day
+                minimum_day = value["days"][minimum_day]
+        return self.least_day
