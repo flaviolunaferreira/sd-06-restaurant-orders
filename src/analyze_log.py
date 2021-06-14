a2771e0110e4646a0e6348709d4722755d88de06
Cássio Rubens <cassiorubensadv@gmail.com>
2021-06-14 14:52:44 -0300

    Starting restaurant order project

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,10 @@
+# Requisito 1
+# A lanchonete quer promover ações de marketing e, para isso,
+# a agência de publicidade precisa exatamente das informações abaixo:
+# Qual o prato mais pedido por 'maria'?
+# Quantas vezes 'arnaldo' pediu 'hamburguer'?
+# Quais pratos 'joao' nunca pediu?
+# Quais dias 'joao' nunca foi na lanchonete?
+
 def analyze_log(path_to_file):
     raise NotImplementedError
