ceee6cd79cf83d4c931cb79cfddd553e58a0baae
Rangel Paolo <paolorangel1996@outlook.com>
2021-06-14 02:52:20 -0300

    I think so

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,75 +1,60 @@
 import csv
 
 
-def read_file_csv(path_to_file):
+def read_file(path):
     try:
-        with open(path_to_file) as path_csv:
-            reader_csv = csv.reader(path_csv, delimiter=",", quotechar='"')
-            orders_list = list(reader_csv)
-
+        with open(path) as file:
+            csv_reader = csv.reader(
+                file, delimiter=",", quotechar='"'
+            )
+            orders = list(csv_reader)
     except FileExistsError:
-        raise ValueError(f"No such file or directory: '{path_to_file}'")
+        raise ValueError(f"No such file or directory: '{path}'")
+    return orders
 
-    return orders_list
 
-
-def most_requested_by_client(orders_list, client):
-    count_orders = {}
-    most_requested = orders_list[0][1]
-    for order in orders_list:
+def find_most_required_by_client(orders, client):
+    famous = orders[0][1]
+    orders_set = {}
+    for order in orders:
         if order[0] == client:
-            if order[1] not in count_orders:
-                count_orders[order[1]] = 1
-            else:
-                count_orders[order[1]] += 1
-
-            if count_orders[order[1]] > count_orders[most_requested]:
-                most_requested = count_orders[order[1]]
-
-    return most_requested
+            if order[1] not in orders_set:
+                orders_set[order[1]] = 1
+            if orders_set[order[1]] > orders_set[famous]:
+                famous = orders_set[order[1]]
+            orders_set[order[1]] += 1
+    return famous
 
 
-def count_asked_food(orders_list, client, food):
-    count_burguers = 0
-    for order in orders_list:
+def count_food(orders, client, food):
+    return sum(
+        [1 for order in orders if order[0] == client and order[1] == food]
+    )
+
+
+def count_not_done(orders, client, choice):
+    orders_set = set()
+    sub_orders = set()
+    for order in orders:
+        orders_set.add(order[choice])
         if order[0] == client:
-            if order[1] == food:
-                count_burguers += 1
-
-    return count_burguers
-
-
-def never_done(orders_list, client, food_or_day):
-    super_set = set()
-    sub_set = set()
-
-    for order in orders_list:
-        super_set.add(order[food_or_day])
-        if order[0] == client:
-            sub_set.add(order[food_or_day])
-
-    client_never_done = super_set.difference(sub_set)
-    return client_never_done
+            sub_orders.add(order[choice])
+    return orders_set.difference(sub_orders)
 
 
 def analyze_log(path_to_file):
-    orders_list = read_file_csv(path_to_file)
-
-    most_requested_by_maria = most_requested_by_client(orders_list, "maria")
-
-    arnaldo_ask_hamburguer = count_asked_food(
-        orders_list, "arnaldo", "hamburguer"
+    orders = read_file(path_to_file)
+    maria_request = find_most_required_by_client(orders, "maria")
+    arnaldo_request = count_food(
+        orders, "arnaldo", "hamburguer"
     )
-
-    joao_never_asked = never_done(orders_list, "joao", 1)
-
-    joao_never_went = never_done(orders_list, "joao", 2)
-
-    with open("data/mkt_campaign.txt", "w") as log:
-        log.write(f"{most_requested_by_maria}\n")
-        log.write(f"{arnaldo_ask_hamburguer}\n")
-        log.write(f"{joao_never_asked}\n")
-        log.write(f"{joao_never_went}\n")
+    joao_request = count_not_done(orders, "joao", 1)
+    joao_frequence = count_not_done(orders, "joao", 2)
+    with open("data/mkt_campaign.txt", "w") as file:
+        file.write(f"{maria_request}\n")
+        file.write(f"{arnaldo_request}\n")
+        file.write(f"{joao_request}\n")
+        file.write(f"{joao_frequence}\n")
 
 
 # print()
