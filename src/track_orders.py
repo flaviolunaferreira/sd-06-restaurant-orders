97f322d62360d3a5f6f75f3def00fbe2689a9f94
Vitor Buxbaum Orlandi <vitorbuxbaum@gmail.com>
2021-06-15 18:42:13 -0300

    remove assinatura de método desnecessário

--- src/track_orders.py
+++ src/track_orders.py
@@ -8,9 +8,6 @@ class TrackOrders:
     def get_most_ordered_dish_per_costumer(self, costumer):
         pass
 
-    def get_order_frequency_per_costumer(self, costumer, order):
-        pass
-
     def get_never_ordered_per_costumer(self, costumer):
         pass
 
