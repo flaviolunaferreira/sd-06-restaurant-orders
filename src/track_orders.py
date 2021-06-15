28e2fa08f99eac22c103c2e7dec588c75b6fbe6f
bynho25 <pablo.trybe@gmail.com>
2021-06-15 00:23:37 -0300

    Finishing the 80's

--- src/track_orders.py
+++ src/track_orders.py
@@ -24,10 +24,24 @@ class TrackOrders:
         pass
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        client_order = set()
+        all_orders = set()
+        for item in self.orders:
+            all_orders.add(item[1])
+        for item in self.orders:
+            if item[0] == costumer:
+                client_order.add(item[1])
+        return all_orders.difference(client_order)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        client_days = set()
+        days = set()
+        for day in self.orders:
+            days.add(day[2])
+        for client in self.orders:
+            if client[0] == costumer:
+                client_days.add(client[2])
+        return days.difference(client_days)
 
     def get_busiest_day(self):
         pass
