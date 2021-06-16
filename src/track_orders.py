313bfe609a9105fbf1d71a761799f4c8aa40dc8d
VitorSSMoreira <vitorssmoreira@gmail.com>
2021-06-15 22:13:17 -0300

    flake8 corrections

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,17 +1,15 @@
 from collections import Counter
 
+
 class TrackOrders:
     def __init__(self):
         self.orders = []
 
-
     def __len__(self):
         return len(self.orders)
 
-
     def add_new_order(self, costumer, order, day):
         self.orders.append([costumer, order, day])
-
 
     def get_most_ordered_dish_per_costumer(self, costumer):
         costumer_orders = []
@@ -22,14 +20,12 @@ class TrackOrders:
             favorite = Counter(costumer_orders).most_common(1)[0][0]
             return favorite
 
-
     def get_order_frequency_per_costumer(self, costumer, order):
         order_frequency = 0
         for item in self.orders:
             if item[0] == costumer and item[1] == order:
                 order_frequency += 1
         return order_frequency
-
 
     def get_never_ordered_per_costumer(self, costumer):
         all_meals = set()
@@ -41,7 +37,6 @@ class TrackOrders:
                 ordered_meal.add(item[1])
         return all_meals.difference(ordered_meal)
 
-
     def get_days_never_visited_per_costumer(self, costumer):
         all_days = set()
         visited_days = set()
@@ -52,14 +47,12 @@ class TrackOrders:
                 visited_days.add(client[2])
         return all_days.difference(visited_days)
 
-
     def get_busiest_day(self):
         working_days = []
         for day in self.orders:
             working_days.append(day[2])
         busiest_day = Counter(working_days).most_common(1)[0][0]
         return busiest_day
-
 
     def get_least_busy_day(self):
         working_days = []
