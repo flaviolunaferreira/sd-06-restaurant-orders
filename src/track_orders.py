1b8b29b98041203aafffa21689bd74d9e7722d7e
William Gomes <williamjog@hotmail.com>
2021-06-15 21:42:49 -0300

    arrumando 2º Requisito

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,75 +1,91 @@
+def plates(self):
+    plates = set()
+    for order in self.orders:
+        plates.add(order["order"])
+    return plates
+
+
+def wishes_by_costumer(self, costumer):
+    food_count = {}
+    for order in self.orders:
+        client = order["costumer"]
+        food = order["order"]
+        if client == costumer:
+            if food not in food_count:
+                food_count[food] = 1
+            else:
+                food_count[food] += 1
+    return food_count
+
+
+def plates_by_client(self, costumer):
+    plates = set()
+    for order in self.orders:
+        client = order["costumer"]
+        if client == costumer:
+            plates.add(order["order"])
+    return plates
+
+
+def days_visited_by_client(self, costumer):
+    days = set()
+    for order in self.orders:
+        client = order["costumer"]
+        if client == costumer:
+            days.add(order["day"])
+    return days
+
+
+def working_days(self):
+    days = set()
+    for order in self.orders:
+        days.add(order["day"])
+    return days
+
+
+def frequency_days(self):
+    day_counter = {}
+    for order in self.orders:
+        day = order["day"]
+        if day not in day_counter:
+            day_counter[day] = 1
+        else:
+            day_counter[day] += 1
+    return day_counter
+
+
 class TrackOrders:
     def __init__(self):
-        self.orders_file = list()
+        self.orders = []
 
     def __len__(self):
-        return len(self.orders_file)
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        self.orders_file.append([costumer, order, day])
+        self.orders.append({"costumer": costumer, "order": order, "day": day})
 
-    def get_most_ordered_dish(self, costumer):
-        highest_orders = ""
-        orders = {}
-        for name, order, day in self.orders_file:
-            if name == costumer:
-                if order not in orders:
-                    orders[order] = 1
-                else:
-                    orders[order] += 1
-                if (
-                    highest_orders not in orders
-                    or orders[highest_orders] < orders[order]
-                ):
-                    highest_orders = order
-        return highest_orders
+    def get_most_ordered_dish_per_costumer(self, costumer):
+        food_count = wishes_by_costumer(self, costumer)
+        return max(food_count, key=food_count.get)
 
-    def get_frequency_by_customer(self, costumer, order):
-        count_order = 0
-        for request in self.orders_file:
-            if request[0] == costumer:
-                if request[1] == order:
-                    count_order += 1
-        return count_order
+    def get_order_frequency_per_costumer(self, costumer, order):
+        food_count = wishes_by_costumer(self, costumer)
+        return food_count[order]
 
-    def do_not_have_order(self, client, dish):
-        super_set = set()
-        sub_set = set()
-        for order in self.orders_file:
-            super_set.add(order[dish])
-            if order[0] == client:
-                sub_set.add(order[dish])
-        dont_have_order = super_set.difference(sub_set)
-        return dont_have_order
-
-    def never_oredered(self, costumer):
-        never_ordered = self.do_not_have_order(costumer, 1)
-        return never_ordered
+    def get_never_ordered_per_costumer(self, costumer):
+        available_plates = plates(self)
+        plates_client = plates_by_client(self, costumer)
+        return available_plates.difference(plates_client)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        never_visited = self.do_not_have_order(costumer, 2)
-        return never_visited
+        days_opened = working_days(self)
+        days_visited = days_visited_by_client(self, costumer)
+        return days_opened.difference(days_visited)
 
     def get_busiest_day(self):
-        count_days = {}
-        busiest_day = self.orders_file[0][2]
-        for order in self.orders_file:
-            if order[2] not in count_days:
-                count_days[order[2]] = 1
-            else:
-                count_days[order[2]] += 1
-            if count_days[order[2]] > count_days[busiest_day]:
-                busiest_day = count_days[order[2]]
-        return busiest_day
+        days_count = frequency_days(self)
+        return max(days_count, key=days_count.get)
 
     def get_least_busy_day(self):
-        count_days = {}
-        least_busy_day = self.orders_file[0][2]
-        for order in self.orders_file:
-            if order[2] not in count_days:
-                count_days[order[2]] = 1
-            else:
-                count_days[order[2]] += 1
-            if count_days[order[2]] <= count_days[least_busy_day]:
-                least_busy_day = order[2]
-        return least_busy_day
+        days_count = frequency_days(self)
+        return min(days_count, key=days_count.get)
