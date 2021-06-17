711a74715197963960e26a0212d6cb196a2b0e62
EliasCarvalho20 <eliascarvalho01@hotmail.com>
2021-06-17 13:00:33 -0300

    projeto restaurant orders

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,34 @@
+import csv
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    with open(path_to_file, "r") as f:
+        lanchonete = list(csv.DictReader(f))
+
+    maria = [l for l in lanchonete if l["maria"] == "maria"]
+    arnaldo = [l for l in lanchonete if l["maria"] == "arnaldo"]
+    joao = [l for l in lanchonete if l["maria"] == "joao"]
+
+    maria_plates = count_maria_plates(maria)
+    arnaldo_plates = sum(d["hamburguer"] == "hamburguer" for d in arnaldo)
+
+    all_meals = {l["hamburguer"] for l in lanchonete}
+    all_days = {l["terça-feira"] for l in lanchonete}
+
+    joao_foods = all_meals.difference([d["hamburguer"] for d in joao])
+    joao_days = all_days.difference([d["terça-feira"] for d in joao])
+
+    with open("data/mkt_campaign.txt", "w") as f:
+        f.write(
+            f"{maria_plates}\n{arnaldo_plates}\n{joao_foods}\n" f"{joao_days}"
+        )
+
+
+def count_maria_plates(maria):
+    counter = {}
+
+    for d in maria:
+        plate = d["hamburguer"]
+        counter[plate] = (counter.get(plate) or 0) + 1
+
+    return max(counter, key=lambda k: counter[k])
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,18 +1,30 @@
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+        self.all_clients = None
+        self.all_meals = None
+        self.all_days = None
+
     def __len__(self):
-        pass
+        return len(self.orders)
+
+    def get_order_by_name(self, name):
+        return [l["order"] for l in self.orders if l["name"] == name]
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append({"name": costumer, "order": order, "day": day})
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        result = self.get_order_by_name(costumer)
+        return max(set(result), key=result.count)
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        result = set(self.get_order_by_name(costumer))
+        return {l["order"] for l in self.orders}.difference(result)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        result = [l["day"] for l in self.orders if l["name"] == costumer]
+        return {l["day"] for l in self.orders}.difference(result)
 
     def get_busiest_day(self):
         pass
