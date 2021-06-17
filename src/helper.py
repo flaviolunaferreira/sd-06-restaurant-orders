0942dd3fb6e711ed39ce29aa783afd4a5b95e90c
higoramorim <1810042@aluno.univesp.br>
2021-06-17 20:48:53 -0300

    fear analyse 2

--- src/helper.py
+++ src/helper.py
@@ -1,12 +1,14 @@
 import csv
 
+
 def read_file(path_to_file):
-  with open(path_to_file) as file:
-    read = csv.reader(file)
-    text = [item for item in read]
-  return text
+    with open(path_to_file) as file:
+        read = csv.reader(file)
+        text = [item for item in read]
+    return text
+
 
 def write_file(data_answers, answers):
-  with open(data_answers) as file:
-    for item in answers:
-      file.write(str(item) + "\n")
+    with open(data_answers) as file:
+        for item in answers:
+            file.write(str(item) + "\n")
