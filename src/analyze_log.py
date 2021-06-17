9bae1f2701471088a088ce91a2763185c7e88549
LeandroParisi <lepcbelisario@gmail.com>
2021-06-17 15:11:27 -0300

    feat: requirement 1

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,6 +1,58 @@
 import csv
-from entities.restaurant import Restaurant
-from entities.customer_orders import CustomerOrders
+
+
+class CustomerOrders:
+    def __init__(self, name, orders):
+        self._name = name
+        self._orders = self.extract_orders(orders)
+
+    def extract_orders(self, orders):
+        extracted_orders = []
+        for person, order, day in orders:
+            if person == self._name:
+                extracted_orders.append([order, day])
+            else:
+                continue
+        return extracted_orders
+
+    def get_orders(self):
+        return self._orders
+
+    def product_total_orders(self, product_name):
+        product_total = 0
+        for product, _day in self._orders:
+            if product == product_name:
+                product_total += 1
+            else:
+                continue
+
+        return product_total
+
+    def most_ordered_dish(self):
+        most_ordered = {}
+        for product, _day in self._orders:
+            most_ordered[product] = most_ordered.get(product, 0) + 1
+        return max(most_ordered, key=most_ordered.get)
+
+    def never_ordered_dish(self, menu):
+        ordered_dishes = {order[0] for order in self._orders}
+        return menu - ordered_dishes
+
+    def days_never_visited(self, work_days):
+        visited_days = {order[1] for order in self._orders}
+        return work_days - visited_days
+
+
+class Restaurant:
+    def __init__(self, orders):
+        self._products = {item[1] for item in orders}
+        self._work_days = {item[2] for item in orders}
+
+    def get_menu(self):
+        return self._products
+
+    def get_work_days(self):
+        return self._work_days
 
 
 def write_csv_to_file(path_to_file, analyzed_log):
@@ -16,10 +68,7 @@ def extract_file_content(path_to_file):
 
 def analyze_log(path_to_file):
     analyzed_log = []
-    try:
-        orders = extract_file_content(path_to_file)
-    except FileNotFoundError:
-        raise FileNotFoundError(f"No such file or directory: {path_to_file}")
+    orders = extract_file_content(path_to_file)
 
     restaurant = Restaurant(orders)
 
@@ -41,7 +90,7 @@ def analyze_log(path_to_file):
     analyzed_log.append(joao_nevers_ordered)
     analyzed_log.append(joao_nevers_went_on)
 
-    file_to_write = "../data/mkt_campaign.txt"
+    file_to_write = "data/mkt_campaign.txt"
     write_csv_to_file(file_to_write, analyzed_log)
 
 
