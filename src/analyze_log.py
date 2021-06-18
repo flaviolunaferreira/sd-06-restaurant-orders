dc299791ebe4b4072e14aa1b1bd68691ee312f09
Fernando Luckesi <fernando.luckesi@94gmail.com>
2021-06-17 22:04:27 -0300

    Requisito 1 em desenvolvimento

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,6 +1,28 @@
 import csv
 
 
+def list_maria(list_orders):
+    orders_list_maria = []
+    for order in list_orders:
+        if order["client"] == "maria":
+            orders_list_maria.append(order["dish"])
+    return orders_list_maria
+
+
+def most_maria(list_orders):
+    orders_list_maria = list_maria(list_orders)
+    dict_dish_maria = {}
+    dish_maria_most_frequent = orders_list_maria[0]
+    for order in orders_list_maria:
+        if order not in dict_dish_maria:
+            dict_dish_maria[order] = 1
+        else:
+            dict_dish_maria[order] += 1
+        if dict_dish_maria[order] > dict_dish_maria[dish_maria_most_frequent]:
+            dish_maria_most_frequent = order
+    return dish_maria_most_frequent
+
+
 def analyze_log(path_to_file):
     with open(path_to_file) as orders_file:
         data = csv.reader(orders_file, delimiter=",")
@@ -10,26 +32,8 @@ def analyze_log(path_to_file):
                 {"client": row[0], "dish": row[1], "day": row[2]}
             )
 
-        orders_list_maria = []
-        for order in list_orders:
-            if order["client"] == "maria":
-                orders_list_maria.append(order["dish"])
-
-        dict_dish_maria = {}
-        dish_maria_most_frequent = orders_list_maria[0]
-        for order in orders_list_maria:
-            if order not in dict_dish_maria:
-                dict_dish_maria[order] = 1
-            else:
-                dict_dish_maria[order] += 1
-            if (
-                dict_dish_maria[order]
-                > dict_dish_maria[dish_maria_most_frequent]
-            ):
-                dish_maria_most_frequent = order
-
         track_orders_file = open("../data/mkt_campaign.txt", mode="w")
-        all_write_track_orders = [f"{dish_maria_most_frequent}\n"]
+        all_write_track_orders = [f"{most_maria(list_orders)}\n"]
         track_orders_file.writelines(all_write_track_orders)
         track_orders_file.close()
 
