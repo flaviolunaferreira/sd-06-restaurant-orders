c8d4f0b858f0d82a7786041b6eafa26022e81a05
Fernando Luckesi <fernando.luckesi@94gmail.com>
2021-06-18 11:34:11 -0300

    Requisito 2 finalizado

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,61 +1,97 @@
 import csv
 
 
-def list_maria(list_orders):
-    orders_list_maria = []
+def list_orders_tranform(data):
+    list_orders = []
+    for row in data:
+        list_orders.append({"client": row[0], "dish": row[1], "day": row[2]})
+    return list_orders
+
+
+def orders_list_dish_per_client(list_orders, client):
+    orders_list = []
     for order in list_orders:
-        if order["client"] == "maria":
-            orders_list_maria.append(order["dish"])
-    return orders_list_maria
+        if order["client"] == client:
+            orders_list.append(order["dish"])
+    return orders_list
 
 
-def most_maria(list_orders):
-    orders_list_maria = list_maria(list_orders)
-    dict_dish_maria = {}
-    dish_maria_most_frequent = orders_list_maria[0]
-    for order in orders_list_maria:
-        if order not in dict_dish_maria:
-            dict_dish_maria[order] = 1
+def order_most_frequent_per_client(data, client):
+    list_orders = list_orders_tranform(data)
+    orders_list_client = orders_list_dish_per_client(list_orders, client)
+    dict_dish_client = {}
+    dish_client_most_frequent = orders_list_client[0]
+    for order in orders_list_client:
+        if order not in dict_dish_client:
+            dict_dish_client[order] = 1
         else:
-            dict_dish_maria[order] += 1
-        if dict_dish_maria[order] > dict_dish_maria[dish_maria_most_frequent]:
-            dish_maria_most_frequent = order
-    return dish_maria_most_frequent
+            dict_dish_client[order] += 1
+        if (
+            dict_dish_client[order]
+            > dict_dish_client[dish_client_most_frequent]
+        ):
+            dish_client_most_frequent = order
+    return dish_client_most_frequent
 
 
-def count_arnaldo(list_orders):
+def count_dish_per_client(data, client, dish):
+    list_orders = list_orders_tranform(data)
     count_arnaldo_hamburguer = 0
     for order in list_orders:
-        if order["client"] == "arnaldo" and order["dish"] == "hamburguer":
+        if order["client"] == client and order["dish"] == dish:
             count_arnaldo_hamburguer += 1
     return count_arnaldo_hamburguer
 
 
-def diffs_sets_joao(list_orders, key):
+def sets_diff_per_client(data, key, client):
+    list_orders = list_orders_tranform(data)
     keys_set_all = set()
     keys_set_joao = set()
     for order in list_orders:
         keys_set_all.add(order[key])
-        if order["client"] == "joao":
+        if order["client"] == client:
             keys_set_joao.add(order[key])
     diffs = keys_set_all.difference(keys_set_joao)
     return diffs
 
 
+def count_days(list_orders):
+    list_days = []
+    dict_count_days = {}
+    for order in list_orders:
+        list_days.append(order["day"])
+    for day in list_days:
+        if day not in dict_count_days:
+            dict_count_days[day] = 1
+        else:
+            dict_count_days[day] += 1
+    return dict_count_days
+
+
+def busiest_days(data):
+    list_orders = list_orders_tranform(data)
+    max_day = max(count_days(list_orders))
+    return max_day
+
+
+def less_busy_days(data):
+    list_orders = list_orders_tranform(data)
+    min_days = min(count_days(list_orders))
+    return min_days
+
+
 def analyze_log(path_to_file):
     with open(path_to_file, "r") as orders_file:
         data = csv.reader(orders_file, delimiter=",")
-        list_orders = []
+        list_data = []
         for row in data:
-            list_orders.append(
-                {"client": row[0], "dish": row[1], "day": row[2]}
-            )
+            list_data.append([row[0], row[1], row[2]])
 
     with open("data/mkt_campaign.txt", "w") as mkt_file:
         all_write_analyze_log = [
-            f"{most_maria(list_orders)}\n",
-            f"{count_arnaldo(list_orders)}\n",
-            f"{diffs_sets_joao(list_orders, 'dish')}\n",
-            f"{diffs_sets_joao(list_orders, 'day')}",
+            f"{order_most_frequent_per_client(list_data, 'maria')}\n",
+            f"{count_dish_per_client(list_data, 'arnaldo', 'hamburguer')}\n",
+            f"{sets_diff_per_client(list_data, 'dish', 'joao')}\n",
+            f"{sets_diff_per_client(list_data, 'day', 'joao')}",
         ]
         mkt_file.writelines(all_write_analyze_log)
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,30 @@
+from src.analyze_log import order_most_frequent_per_client
+from src.analyze_log import sets_diff_per_client
+from src.analyze_log import busiest_days
+from src.analyze_log import less_busy_days
+
+
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append([costumer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        return order_most_frequent_per_client(self.orders, costumer)
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        return sets_diff_per_client(self.orders, "dish", costumer)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        return sets_diff_per_client(self.orders, "dish", costumer)
 
     def get_busiest_day(self):
-        pass
+        return busiest_days(self.orders)
 
     def get_least_busy_day(self):
-        pass
+        return less_busy_days(self.orders)
