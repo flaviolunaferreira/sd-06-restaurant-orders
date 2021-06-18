0f5be2d9fd5e86a31e412669a60b222510c4565f
paulo-lins <paulolins100@gmail.com>
2021-06-17 23:41:08 -0300

    fix

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,27 +1,59 @@
-from src.utils.read_csv import read_csv_file
-from src.reports.favorite_meal import favorite_meal
-from src.reports.meal_quantity import meal_quantity_by_client
-from src.reports.never_ordered import never_ordered_meals
-from src.reports.days_client_not import days_client_not_here
+import csv
+from collections import Counter
+
+
+def leitor_csv(path):
+    if not path.endswith(".csv"):
+        raise FileNotFoundError(f"No such file or directory: '{path}'")
+
+    with open(path) as file:
+        return list(csv.reader(file))
+
+
+def comida_favorita(name, orders):
+    client_orders = []
+    if len(orders) > 0:
+        for item in orders:
+            if item[0] == name:
+                client_orders.append(item[1])
+        return Counter(client_orders).most_common(1)[0][0]
+
+
+def repeticoes_pedido(name, meal, orders):
+    contador = 0
+    for item in orders:
+        if item[0] == name and item[1] == meal:
+            contador += 1
+    return contador
+
+
+def nunca_pedido(name, orders):
+    pedido_cliente = set()
+    todos_pedidos = set()
+    for item in orders:
+        todos_pedidos.add(item[1])
+    for item in orders:
+        if item[0] == name:
+            pedido_cliente.add(item[1])
+    return todos_pedidos.difference(pedido_cliente)
+
+
+def dias_sem_pedido(name, days):
+    dias_cliente = set()
+    dias = set()
+    for day in days:
+        dias.add(day[2])
+    for client in days:
+        if client[0] == name:
+            dias_cliente.add(client[2])
+    return dias.difference(dias_cliente)
 
 
 def analyze_log(path_to_file):
-    answers = ""
-    orders = read_csv_file(path_to_file)
-
-    # Qual o prato mais pedido por 'maria'?
-    answers += str(favorite_meal("maria", orders)) + "\n"
-
-    # Quantas vezes 'arnaldo' pediu 'hamburguer'?
-    answers += (
-        str(meal_quantity_by_client("arnaldo", "hamburguer", orders)) + "\n"
-    )
-
-    # Quais pratos 'joao' nunca pediu?
-    answers += str(never_ordered_meals("joao", orders)) + "\n"
-
-    # Quais dias 'joao' nunca foi na lanchonete?
-    answers += str(days_client_not_here("joao", orders))
-
+    data = leitor_csv(path_to_file)
+    favorite = comida_favorita('maria', data)
+    times = repeticoes_pedido('arnaldo', 'hamburguer', data)
+    not_ordered = nunca_pedido('joao', data)
+    days = dias_sem_pedido('joao', data)
     with open("data/mkt_campaign.txt", "w") as file:
-        file.write(answers)
+        file.write(f'{favorite}\n{times}\n{not_ordered}\n{days}')
