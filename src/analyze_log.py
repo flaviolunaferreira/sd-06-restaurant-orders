99eac95eee8a6780972bc57a3a0e0ad7f1fb791c
Fernando Luckesi <fernando.luckesi@94gmail.com>
2021-06-17 21:07:09 -0300

    Requisito 1 em desenvolvimento

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,37 @@
+import csv
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    with open(path_to_file) as orders_file:
+        data = csv.reader(orders_file, delimiter=",")
+        list_orders = []
+        for row in data:
+            list_orders.append(
+                {"client": row[0], "dish": row[1], "day": row[2]}
+            )
+
+        orders_list_maria = []
+        for order in list_orders:
+            if order["client"] == "maria":
+                orders_list_maria.append(order["dish"])
+
+        dict_dish_maria = {}
+        dish_maria_most_frequent = orders_list_maria[0]
+        for order in orders_list_maria:
+            if order not in dict_dish_maria:
+                dict_dish_maria[order] = 1
+            else:
+                dict_dish_maria[order] += 1
+            if (
+                dict_dish_maria[order]
+                > dict_dish_maria[dish_maria_most_frequent]
+            ):
+                dish_maria_most_frequent = order
+
+        track_orders_file = open("../data/mkt_campaign.txt", mode="w")
+        all_write_track_orders = [f"{dish_maria_most_frequent}\n"]
+        track_orders_file.writelines(all_write_track_orders)
+        track_orders_file.close()
+
+
+analyze_log("../data/orders_1.csv")
