5f8187812768038c675d311d3aab8689f6eb7d47
alcantara2509 <alcantara2509@gmail.com>
2021-06-19 20:58:30 -0300

    alcantara2509

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,10 @@
-def analyze_log(path_to_file):
-    raise NotImplementedError
+import csv
+
+
+def csv_data(path):
+    all_orders = []
+    with open(path) as file:
+        data = csv.DictReader(file, fieldnames=['nome', 'comida', 'dia'])
+        for info in data:
+            all_orders.append(info)
+    return all_orders
