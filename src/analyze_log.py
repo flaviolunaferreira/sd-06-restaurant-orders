29b42df881dbcfbf9917fc56ffca4ae1a9cb1a20
Dilenio Enderle <dilenio@gmail.com>
2021-06-19 18:10:26 -0300

    requests 1 and 2

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,84 @@
+from collections import Counter
+import csv
+
+
+def getdata(path_to_file):
+    with open(path_to_file, "r") as file:
+        keys = ["name", "order", "day"]
+        data = csv.DictReader(file, fieldnames=keys)
+        customers = []
+        [customers.append(row) for row in data]
+    return customers
+
+
+def most_frequent_order(customer_name, customers_data):
+    customer_order = [
+        customer["order"]
+        for customer in customers_data
+        if customer["name"] == customer_name
+    ]
+    count = Counter(customer_order)
+    frequent = count.most_common(1)[0][0]
+    return frequent
+
+
+def order_number(customer_name, order, customers_data):
+    customer_order = [
+        customer["order"]
+        for customer in customers_data
+        if customer["name"] == customer_name
+    ]
+    occurances = Counter(customer_order)
+    order_count = occurances.get(order)
+    return order_count
+
+
+def order_records(customer_name, customer_data):
+    menu = {customer["order"] for customer in customer_data}
+    orders_customer = {
+        customer["order"]
+        for customer in customer_data
+        if customer["name"] == customer_name
+    }
+    never_ordered = menu.symmetric_difference(orders_customer)
+    return never_ordered
+
+
+def days_records(customer_name, customer_data):
+    order_days = {customer["day"] for customer in customer_data}
+    days_customer = {
+        customer["day"]
+        for customer in customer_data
+        if customer["name"] == customer_name
+    }
+    no_day_record = order_days.symmetric_difference(days_customer)
+    return no_day_record
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    if path_to_file.endswith(".csv"):
+        txt_output = "data/mkt_campaign.txt"
+        client_data = getdata(path_to_file)
+        client_favorite = most_frequent_order("maria", client_data)
+        arnaldo_orders = order_number(
+            "arnaldo", "hamburguer", client_data
+        )
+        joao_records = order_records("joao", client_data)
+        joao_day_records = days_records("joao", client_data)
+        log = [
+            f"{client_favorite}\n",
+            f"{arnaldo_orders}\n",
+            f"{joao_records}\n",
+            f"{joao_day_records}",
+        ]
+        with open(txt_output, "w") as file:
+            file.writelines(log)
+    else:
+        raise FileNotFoundError(
+            f"No such file or directory: '{path_to_file}'"
+        )
+
+
+if __name__ == "__main__":
+    path = "data/orders_3.csv"
+    analyze_log(path)
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,51 @@
+from collections import Counter
+
+
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        return self.orders.append([costumer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        ordered_meals = []
+        for order in self.orders:
+            if order[0] == costumer:
+                ordered_meals.append(order[1])
+        return Counter(ordered_meals).most_common(1)[0][0]
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        meals = set()
+        client_ordered_meals = set()
+        for order in self.orders:
+            meals.add(order[1])
+        for order in self.orders:
+            if order[0] == costumer:
+                client_ordered_meals.add(order[1])
+        return meals.difference(client_ordered_meals)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        days = set()
+        client_ordered_days = set()
+        for order in self.orders:
+            days.add(order[2])
+        for order in self.orders:
+            if order[0] == costumer:
+                client_ordered_days.add(order[2])
+        return days.difference(client_ordered_days)
 
     def get_busiest_day(self):
-        pass
+        days = []
+        for order in self.orders:
+            days.append(order[2])
+        return Counter(days).most_common(1)[0][0]
 
     def get_least_busy_day(self):
-        pass
+        days = []
+        for order in self.orders:
+            days.append(order[2])
+        return Counter(days).most_common()[-1][0]
