525d2aded176c121f362892ff1e1e2e734e37333
Renan Cunha <renan2610@gmail.com>
2021-06-19 15:05:54 -0300

    feat: requirement 2

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -5,14 +5,14 @@ def analyze_log(path_to_file):
     data_json = csv_to_json(path_to_file)
     maria = get_maria_most_requested(data_json)
     arnaldo = get_arnaldo_hamburguer_count(data_json)
-    order_joao_never = get_joao_never_did(data_json, 'pedido')
-    day_joao_never = get_joao_never_did(data_json, 'dia')
+    order_joao_never = get_joao_never_did(data_json, 'order')
+    day_joao_never = get_joao_never_did(data_json, 'day')
 
-    file = open("data/mkt_campaign.txt", mode="w")
+    file = open('data/mkt_campaign.txt', mode='w')
 
-    file.write(maria + "\n")
-    file.write(str(arnaldo) + "\n")
-    file.write(str(order_joao_never) + "\n")
+    file.write(maria + '\n')
+    file.write(str(arnaldo) + '\n')
+    file.write(str(order_joao_never) + '\n')
     file.write(str(day_joao_never))
     file.close()
 
@@ -21,7 +21,7 @@ def csv_to_json(filename):
     if '.csv' in filename:
         with open(filename) as file:
             content = csv.DictReader(file)
-            content.fieldnames = ['nome'] + ['pedido'] + ['dia']
+            content.fieldnames = ['name'] + ['order'] + ['day']
             jsonFormat = []
             for info in content:
                 jsonFormat.append(info)
@@ -34,15 +34,15 @@ def get_maria_most_requested(order_data):
     count = {}
     most_requested = {'most_repet': '', 'repet': 1}
     for order in order_data:
-        is_maria = order['nome'] == 'maria'
-        if is_maria and order['pedido'] in count.keys():
-            count[order['pedido']] += 1
-        if is_maria and order['pedido'] not in count.keys():
-            count[order['pedido']] = 1
-        if is_maria and count[order['pedido']] > most_requested['repet']:
+        is_maria = order['name'] == 'maria'
+        if is_maria and order['order'] in count.keys():
+            count[order['order']] += 1
+        if is_maria and order['order'] not in count.keys():
+            count[order['order']] = 1
+        if is_maria and count[order['order']] > most_requested['repet']:
             most_requested = {
-                'most_repet': order['pedido'],
-                'repet': count[order['pedido']]
+                'most_repet': order['order'],
+                'repet': count[order['order']]
             }
 
     return most_requested['most_repet']
@@ -51,7 +51,7 @@ def get_maria_most_requested(order_data):
 def get_arnaldo_hamburguer_count(order_data):
     count = 0
     for order in order_data:
-        if order['nome'] == 'arnaldo' and order['pedido'] == 'hamburguer':
+        if order['name'] == 'arnaldo' and order['order'] == 'hamburguer':
             count += 1
 
     return count
@@ -63,7 +63,7 @@ def get_joao_never_did(order_data, parameter):
     for order in order_data:
         if order[parameter] not in all_list:
             all_list.add(order[parameter])
-        if order['nome'] == 'joao' and order[parameter] not in joao_list:
+        if order['name'] == 'joao' and order[parameter] not in joao_list:
             joao_list.add(order[parameter])
 
     return all_list.difference(joao_list)
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,70 @@
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append({'name': costumer, 'order': order, 'day': day})
+
+    def get_the_most(self, costumer, parameter):
+        count = {}
+        most_req = {'most_repet': '', 'repet': 1}
+        for order in self.orders:
+            is_costumer = order['name'] == costumer if costumer else True
+            if is_costumer and order[parameter] in count.keys():
+                count[order[parameter]] += 1
+            if is_costumer and order[parameter] not in count.keys():
+                count[order[parameter]] = 1
+            if is_costumer and count[order[parameter]] > most_req['repet']:
+                most_req = {
+                    'most_repet': order[parameter],
+                    'repet': count[order[parameter]]
+                }
+
+        return most_req['most_repet']
 
     def get_most_ordered_dish_per_costumer(self, costumer):
+        return self.get_the_most(costumer, 'order')
+
+    def get_costumer_never_did(self, customer, parameter):
+        all_list = set()
+        customer_list = set()
+        for order in self.orders:
+            is_costumer = order['name'] == customer
+            if order[parameter] not in all_list:
+                all_list.add(order[parameter])
+            if is_costumer and order[parameter] not in customer_list:
+                customer_list.add(order[parameter])
+
+        return all_list.difference(customer_list)
+
+    def get_order_frequency_per_costumer(self, costumer, order):
         pass
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        return self.get_costumer_never_did(costumer, 'order')
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        return self.get_costumer_never_did(costumer, 'day')
 
     def get_busiest_day(self):
-        pass
+        return self.get_the_most(False, 'day')
 
     def get_least_busy_day(self):
-        pass
+        count = {}
+        least_day = {'least_repet': '', 'repet': 20}
+        for order in self.orders:
+            if order['day'] in count.keys():
+                count[order['day']] += 1
+            if order['day'] not in count.keys():
+                count[order['day']] = 1
+            update_least = order['day'] == least_day['least_repet']
+            if count[order['day']] < least_day['repet'] or update_least:
+                least_day = {
+                    'least_repet': order['day'],
+                    'repet': count[order['day']]
+                }
+
+        return least_day['least_repet']
