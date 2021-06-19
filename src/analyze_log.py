a224cbae58c87edd62bce92a1882f639d3e71a16
flavio luna ferreira <flaviolunaferreira@gmail.com>
2021-06-19 11:29:29 -0300

    corrigindo primeiro requisito

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,16 +1,18 @@
-from src.aux.csv import csv
-from src.aux.favorite import favorite
-from src.aux.quantity_product import quantity_products as quantities
-from src.aux.aux_date import aux_date as date_
+from src.aux.csv import csv as reader_csv
+from src.aux.favorite import favorite as favorite_food
+from src.aux.quantity_product import quantity_products as qtd_products
+from src.aux.aux_date import aux_date as data
 
 
 def analyze_log(path_to_file):
-    response = []
-    get_csv = csv(path_to_file)
-    response.append(favorite("maria", get_csv))
-    response.append(quantities("arnaldo", "hamburguer", get_csv))
-    response.append(date_("joao", get_csv, "food"))
-    response.append(date_("joao", get_csv, "day"))
+    analized_res = []
+    csv_reader = reader_csv(path_to_file)
+    analized_res.append(favorite_food("maria", csv_reader))
+    analized_res.append(qtd_products("arnaldo", "hamburguer", csv_reader))
+    analized_res.append(data("joao", csv_reader, "food"))
+    analized_res.append(data("joao", csv_reader, "day"))
     file_txt = open("data/mkt_campaign.txt", mode="w")
-    file_txt.writelines(response)
+    file_txt.writelines(analized_res)
     file_txt.close()
+
+# print(analyze_log("data/orders_1.csv"))
