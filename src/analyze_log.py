20c83e470e506efd1bd696fd2b8078825a4d5f1f
vbergadev <vbergadev@gmail.com>
2021-06-20 15:03:08 -0300

    req 1 e 2 passararm local

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,61 @@
+import csv
+from collections import Counter
+
+
+def csv_reader(file_path):
+    with open(file_path) as file:
+        return list(csv.reader(file))
+
+
+def favorite_meal(client, orders):
+    ordered = []
+    for i in orders:
+        if i[0] == client:
+            ordered.append(i[1])
+    return Counter(ordered).most_common(1)[0][0]
+
+
+def meal_ordered_count(client, meal, orders):
+    repeats_order = 0
+    for i in orders:
+        if i[0] == client and i[1] == meal:
+            repeats_order += 1
+    return repeats_order
+
+
+def never_ordered(client, orders):
+    meals_list = set()
+    orders_by_clients = set()
+    for i in orders:
+        meals_list.add(i[1])
+    for i in orders:
+        if i[0] == client:
+            orders_by_clients.add(i[1])
+    return meals_list.difference(orders_by_clients)
+
+
+def days_unordered(client, orders):
+    week_days = set()
+    ordered_days = set()
+    for i in orders:
+        week_days.add(i[2])
+    for i in orders:
+        if i[0] == client:
+            ordered_days.add(i[2])
+    return week_days.difference(ordered_days)
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    orders = csv_reader(path_to_file)
+    client_favorite_meal = favorite_meal('maria', orders)
+    client_meal_ordered_count = meal_ordered_count(
+        'arnaldo', 'hamburguer', orders)
+    client_never_ordered = never_ordered('joao', orders)
+    client_days_unordered = days_unordered('joao', orders)
+    log = (
+        f'{client_favorite_meal}\n'
+        f'{client_meal_ordered_count}\n'
+        f'{client_never_ordered}\n'
+        f'{client_days_unordered}')
+    with open("data/mkt_campaign.txt", "w") as file:
+        file.write(log)
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,48 @@
+from collections import Counter
+
+
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        return self.orders.append([costumer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        ordered = []
+        for i in self.orders:
+            if i[0] == costumer:
+                ordered.append(i[1])
+        return Counter(ordered).most_common(1)[0][0]
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        ordered = []
+        for i in self.orders:
+            if i[0] == costumer:
+                ordered.append(i[1])
+        return Counter(ordered).most_common(1)[0][0]
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        days = set()
+        orders_by_clients = set()
+        for i in self.orders:
+            days.add(i[2])
+        for i in self.orders:
+            if i[0] == costumer:
+                orders_by_clients.add(i[2])
+        return days.difference(orders_by_clients)
 
     def get_busiest_day(self):
-        pass
+        open_days = []
+        for i in self.orders:
+            open_days.append(i[2])
+        return Counter(open_days).most_common(1)[0][0]
 
     def get_least_busy_day(self):
-        pass
+        open_days = []
+        for i in self.orders:
+            open_days.append(i[2])
+        return Counter(open_days).most_common()[-1][0]
