5cf6f0d676321bb0ad6ceeae3350e56ac990a983
alcantara2509 <alcantara2509@gmail.com>
2021-06-19 21:00:47 -0300

    orders by clients

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -3,8 +3,19 @@ import csv
 
 def csv_data(path):
     all_orders = []
+    # if not path.endswith(".csv"):
+    #     raise FileNotFoundError(f"No such file or directory: '{path}'")
     with open(path) as file:
         data = csv.DictReader(file, fieldnames=['nome', 'comida', 'dia'])
         for info in data:
             all_orders.append(info)
     return all_orders
+
+
+def get_orders_by_client(name, orders):
+    orders_by_client = [
+        order['comida']
+        for order in orders
+        if order['nome'] == name
+    ]
+    return orders_by_client
