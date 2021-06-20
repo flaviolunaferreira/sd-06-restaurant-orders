557c1bd5cd69baa28e08cb8dd34eeb227f0a58c8
alcantara2509 <alcantara2509@gmail.com>
2021-06-19 22:11:25 -0300

    req 2.1

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,12 +1,23 @@
+from collections import Counter
+
+
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        self.orders = []
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append([costumer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        if len(self.orders) > 0:
+            client_orders = []
+            for item in self.orders:
+                if item[0] == costumer:
+                    client_orders.append(item[1])
+            return Counter(client_orders).most_common(1)[0][0]
 
     def get_never_ordered_per_costumer(self, costumer):
         pass
