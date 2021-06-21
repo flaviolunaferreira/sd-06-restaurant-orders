5ddf25f66f1fec6ddbc629838b634cec2e28b576
isabellavjs <isabellavjs@gmail.com>
2021-06-21 20:01:10 -0300

    Requirement 4

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -7,7 +7,6 @@ class InventoryControl:
             "coxinha": ["massa", "frango"],
         }
         self.orders = []
-
         self.minimum_inventory = {
             "pao": 50,
             "carne": 50,
@@ -34,10 +33,34 @@ class InventoryControl:
             for ingredient in self.minimum_inventory.keys()
         }
 
+    def get_available_dishes(self):
+        available_ingredients = {
+            ingredient_inventory
+            for ingredient_inventory in self.inventory
+            if self.inventory[ingredient_inventory] != 0
+        }
+        menu = {dish for dish in self.ingredients}
+        ingredients_inventory = set(self.inventory.keys())
+        if ingredients_inventory.issubset(available_ingredients):
+            return menu
+        else:
+            missing_ingredients = ingredients_inventory.symmetric_difference(
+                available_ingredients
+            )
+            for ingredient in missing_ingredients:
+                update_menu = {
+                    dish
+                    for dish in menu
+                    if ingredient not in self.ingredients[dish]
+                }
+            return update_menu
+
 
 if __name__ == "__main__":
     inventory = InventoryControl()
     inventory.add_new_order("jorge", "hamburguer", "terça-feira")
     inventory.add_new_order("jorge", "hamburguer", "terça-feira")
     hamburguer = inventory.get_quantities_to_buy()
-    print(hamburguer)
+    # print(hamburguer)
+    available = inventory.get_available_dishes()
+    print(available)
--- src/main.py
+++ src/main.py
@@ -9,13 +9,13 @@ def print_info(tracker, control):
     print(tracker.get_order_frequency_per_costumer('arnaldo', 'hamburguer'))
     print(tracker.get_never_ordered_per_costumer('joao'))
     print(tracker.get_days_never_visited_per_costumer('joao'))
-    print(control.get_quantities_to_buy())
     print(tracker.get_least_busy_day())
+    print(control.get_available_dishes())
 
 
 def main():
     topic = 'order'
-    path = "data/orders_1.csv"
+    path = "data/orders_2.csv"
 
     tracker = TrackOrders()
     control = InventoryControl()
