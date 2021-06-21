40eba508a1c175c1f205b522baff137e37e2deb9
Vinicius Buzato <viniciusbuzato@gmail.com>
2021-06-20 22:04:59 -0300

    feat: requirement 1

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,5 +1,8 @@
 import csv
-from src.utils.filters import Filters
+from src.utils.filters import most_requested_order
+from src.utils.filters import how_many_orders
+from src.utils.filters import user_never_asked
+from src.utils.filters import user_never_came
 
 
 def csv_reader(csv_path):
@@ -11,14 +14,14 @@ def analyze_log(csv_path):
     data = csv_reader(csv_path)
 
     with open('data/mkt_campaign.txt', 'w') as txt_file:
-        print(Filters.most_requested_order('maria', data), file=txt_file)
+        print(most_requested_order('maria', data), file=txt_file)
 
-        print(Filters.how_many_orders(
+        print(how_many_orders(
             'arnaldo', 'hamburguer', data), file=txt_file)
 
-        print(Filters.user_never_asked('joao', data), file=txt_file)
+        print(user_never_asked('joao', data), file=txt_file)
 
-        print(Filters.user_never_came('joao', data), file=txt_file)
+        print(user_never_came('joao', data), file=txt_file)
 
 
 # analyze_log('data/orders_1.csv')
